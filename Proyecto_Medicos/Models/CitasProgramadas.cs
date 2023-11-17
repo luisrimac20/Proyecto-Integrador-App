18974e1a0c00cc3d131643d39d1b8f9535380e61
luisrimac20 <rimacchavezluis@gmail.com>
2023-11-17 16:58:28 -0500

    Configurnado el formulario de Reprogramar Citas

--- Proyecto_Medicos/Models/CitasProgramadas.cs
+++ Proyecto_Medicos/Models/CitasProgramadas.cs
@@ -1,12 +1,20 @@
-﻿namespace Proyecto_Medicos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Medicos.Models
 {
     public class CitasProgramadas
     {
+        [Display(Name = "Codigo Cita")]
         public int codCita { get; set; }
+        [Display(Name = "Nombres Paciente")]
         public string? nomPac { get; set; }
+        [Display(Name = "Nombre Medico")]
         public string? codMed { get; set; }
+        [Display(Name = "Especialidad")]
         public string? codEsp { get; set; }
+        [Display(Name = "Turno")]
         public string? codTurno { get; set; }
+        [Display(Name = "Fecha Cita")]
         public DateTime fecha { get; set; }
     }
 }
